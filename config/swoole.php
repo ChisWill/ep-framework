4563af323ae6cb37109ddfad814232eddf5532a1
CHISWILL-HOST\freed <freedomyky@163.com>
2021-08-15 22:16:22 +0800

    v0.1

--- config/swoole.php
+++ config/swoole.php
@@ -2,12 +2,12 @@
 
 declare(strict_types=1);
 
-use Ep\Swoole\Server;
+use Ep\Swoole\ServerFactory;
 
 return [
     'host' => '0.0.0.0',
     'port' => 9501,
-    'type' => Server::WEBSOCKET,
+    'type' => ServerFactory::WEBSOCKET,
     'settings' => [
         'worker_num' => 2
     ],
