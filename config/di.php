682d79c6e48321a29e477293038841923e4a1617
CHISWILL-HOST\freed <freedomyky@163.com>
2021-10-05 16:31:33 +0800

    v0.1.3

--- config/di.php
+++ config/di.php
@@ -4,9 +4,18 @@ declare(strict_types=1);
 
 use Ep\Base\Config;
 use Yiisoft\Aliases\Aliases;
+use Yiisoft\Db\Connection\Connection;
+use Yiisoft\Db\Mysql\Connection as MysqlConnection;
 use Yiisoft\Log\Target;
 use Yiisoft\Log\Target\File\FileTarget;
 
 return static fn (Config $config): array => [
     Target::class => static fn (Aliases $aliases): FileTarget => new FileTarget($aliases->get($config->runtimeDir . '/logs/' . date('Y-m-d') . '.log')),
+    // Default DB
+    Connection::class => [
+        'class' => MysqlConnection::class,
+        '__construct()' => [env('MYSQL_DSN')],
+        'setUsername()' => [env('MYSQL_USERNAME')],
+        'setPassword()' => [env('MYSQL_PASSWORD')]
+    ],
 ];
